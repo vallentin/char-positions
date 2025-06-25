361aedfd3bcc9bc98e0af8416441f4e56e9074ea
vallentin <mail@vallentin.dev>
2025-06-26 01:03:16 +0200

    Added docs and readme

---
+++ README.md
@@ -0,0 +1,124 @@
+# char-positions
+
+<!-- cargo-rdme start -->
+
+Similar to the standard library's [`.char_indicies()`], but instead of only
+producing the start byte position. This library implements [`.char_positions()`],
+which can produce any combination of line, column, start byte, and end byte position.
+
+As an example use
+<code>text.[char_positions]::&lt;[LineCol]&gt;()</code>
+to get the line and column of each [`char`].
+Use [`LineColByteRange`] to additionally get the byte range,
+or just [`Line`] to simply get the line number.
+
+### Example
+
+```rust
+use char_positions::{CharPositionsExt, LineCol};
+
+let text = "Hello 👋\nWorld 🌏\n🦀🦀";
+
+for (LineCol(line, col), c) in text.char_positions() {
+    println!("[Ln {line}, Col {col}] {c:?}");
+}
+```
+
+Which will output:
+
+```text
+[Ln 1, Col 1] 'H'
+[Ln 1, Col 2] 'e'
+[Ln 1, Col 3] 'l'
+[Ln 1, Col 4] 'l'
+[Ln 1, Col 5] 'o'
+[Ln 1, Col 6] ' '
+[Ln 1, Col 7] '👋'
+[Ln 1, Col 8] '\n'
+[Ln 2, Col 1] 'W'
+[Ln 2, Col 2] 'o'
+[Ln 2, Col 3] 'r'
+[Ln 2, Col 4] 'l'
+[Ln 2, Col 5] 'd'
+[Ln 2, Col 6] ' '
+[Ln 2, Col 7] '🌏'
+[Ln 2, Col 8] '\n'
+[Ln 3, Col 1] '🦀'
+[Ln 3, Col 2] '🦀'
+```
+
+### Supported
+
+| `.char_positions::<T>()` | Produces |
+|:---|---|
+| [`usize`] | Start byte index (same as [`.char_indicies()`]) |
+| [`std::ops::Range<usize>`] | Start byte and end byte index, i.e. `&text[range]` is the `char` |
+| [`LineColByteRange`] | Line number, column number, and byte range |
+| [`LineCol`] | Line number and column number |
+| [`Line`] | Line number |
+| [`Col`] | Column number |
+| [`ByteRange`] | Same as using [`std::ops::Range<usize>`] |
+| [`ByteStart`] | Start byte index (same as [`.char_indicies()`]) |
+| [`ByteEnd`] | End byte index |
+| _Tuples are also supported, e.g._ | |
+| <code>([Line],)</code> | _Produces the tuple_ |
+| <code>([Line], [Col])</code> | _Produces the tuple_ |
+| <code>([Line], [Col], [ByteStart], [ByteEnd])</code> | _Produces the tuple_ |
+| _etc._ | |
+
+### Example - `LineColByteRange`
+
+```rust
+use char_positions::{CharPositionsExt, LineColByteRange};
+
+let text = "Hello 👋\nWorld 🌏\n🦀🦀";
+
+let mut iter = text
+    .char_positions::<LineColByteRange>()
+    .map(|(LineColByteRange(line, col, range), c)| (line, col, range, c));
+
+assert_eq!(iter.next(), Some((1, 1, 0..1, 'H')));
+assert_eq!(iter.next(), Some((1, 2, 1..2, 'e')));
+assert_eq!(iter.next(), Some((1, 3, 2..3, 'l')));
+assert_eq!(iter.next(), Some((1, 4, 3..4, 'l')));
+assert_eq!(iter.next(), Some((1, 5, 4..5, 'o')));
+assert_eq!(iter.next(), Some((1, 6, 5..6, ' ')));
+assert_eq!(iter.next(), Some((1, 7, 6..10, '👋')));
+assert_eq!(iter.next(), Some((1, 8, 10..11, '\n')));
+assert_eq!(iter.next(), Some((2, 1, 11..12, 'W')));
+assert_eq!(iter.next(), Some((2, 2, 12..13, 'o')));
+assert_eq!(iter.next(), Some((2, 3, 13..14, 'r')));
+assert_eq!(iter.next(), Some((2, 4, 14..15, 'l')));
+assert_eq!(iter.next(), Some((2, 5, 15..16, 'd')));
+assert_eq!(iter.next(), Some((2, 6, 16..17, ' ')));
+assert_eq!(iter.next(), Some((2, 7, 17..21, '🌏')));
+assert_eq!(iter.next(), Some((2, 8, 21..22, '\n')));
+assert_eq!(iter.next(), Some((3, 1, 22..26, '🦀')));
+assert_eq!(iter.next(), Some((3, 2, 26..30, '🦀')));
+assert_eq!(iter.next(), None);
+```
+
+
+[`.char_positions()`]: https://docs.rs/char-positions/*/char_positions/trait.CharPositionsExt.html#tymethod.char_positions
+[char_positions]: https://docs.rs/char-positions/*/char_positions/trait.CharPositionsExt.html#tymethod.char_positions
+
+[`LineColByteRange`]: https://docs.rs/char-positions/*/char_positions/struct.LineColByteRange.html
+[`LineCol`]: https://docs.rs/char-positions/*/char_positions/struct.LineCol.html
+[`Line`]: https://docs.rs/char-positions/*/char_positions/struct.Line.html
+[`Col`]: https://docs.rs/char-positions/*/char_positions/struct.Line.html
+[`ByteRange`]: https://docs.rs/char-positions/*/char_positions/struct.ByteRange.html
+[`ByteStart`]: https://docs.rs/char-positions/*/char_positions/struct.ByteStart.html
+[`ByteEnd`]: https://docs.rs/char-positions/*/char_positions/struct.ByteEnd.html
+
+[LineColByteRange]: https://docs.rs/char-positions/*/char_positions/struct.LineColByteRange.html
+[LineCol]: https://docs.rs/char-positions/*/char_positions/struct.LineCol.html
+[Line]: https://docs.rs/char-positions/*/char_positions/struct.Line.html
+[Col]: https://docs.rs/char-positions/*/char_positions/struct.Col.html
+[ByteStart]: https://docs.rs/char-positions/*/char_positions/struct.ByteStart.html
+[ByteEnd]: https://docs.rs/char-positions/*/char_positions/struct.ByteEnd.html
+
+[`.char_indicies()`]: https://doc.rust-lang.org/std/primitive.str.html#method.char_indices
+
+[`std::ops::Range<usize>`]: https://doc.rust-lang.org/std/ops/struct.Range.html
+
+<!-- cargo-rdme end -->
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,123 @@
+//! Similar to the standard library's [`.char_indicies()`], but instead of only
+//! producing the start byte position. This library implements [`.char_positions()`],
+//! which can produce any combination of line, column, start byte, and end byte position.
+//!
+//! As an example use
+//! <code>text.[char_positions]::&lt;[LineCol]&gt;()</code>
+//! to get the line and column of each [`char`].
+//! Use [`LineColByteRange`] to additionally get the byte range,
+//! or just [`Line`] to simply get the line number.
+//!
+//! ## Example
+//!
+//! ```
+//! use char_positions::{CharPositionsExt, LineCol};
+//!
+//! let text = "Hello 👋\nWorld 🌏\n🦀🦀";
+//!
+//! for (LineCol(line, col), c) in text.char_positions() {
+//!     println!("[Ln {line}, Col {col}] {c:?}");
+//! }
+//! ```
+//!
+//! Which will output:
+//!
+//! ```text
+//! [Ln 1, Col 1] 'H'
+//! [Ln 1, Col 2] 'e'
+//! [Ln 1, Col 3] 'l'
+//! [Ln 1, Col 4] 'l'
+//! [Ln 1, Col 5] 'o'
+//! [Ln 1, Col 6] ' '
+//! [Ln 1, Col 7] '👋'
+//! [Ln 1, Col 8] '\n'
+//! [Ln 2, Col 1] 'W'
+//! [Ln 2, Col 2] 'o'
+//! [Ln 2, Col 3] 'r'
+//! [Ln 2, Col 4] 'l'
+//! [Ln 2, Col 5] 'd'
+//! [Ln 2, Col 6] ' '
+//! [Ln 2, Col 7] '🌏'
+//! [Ln 2, Col 8] '\n'
+//! [Ln 3, Col 1] '🦀'
+//! [Ln 3, Col 2] '🦀'
+//! ```
+//!
+//! ## Supported
+//!
+//! | `.char_positions::<T>()` | Produces |
+//! |:---|---|
+//! | [`usize`] | Start byte index (same as [`.char_indicies()`]) |
+//! | [`std::ops::Range<usize>`] | Start byte and end byte index, i.e. `&text[range]` is the `char` |
+//! | [`LineColByteRange`] | Line number, column number, and byte range |
+//! | [`LineCol`] | Line number and column number |
+//! | [`Line`] | Line number |
+//! | [`Col`] | Column number |
+//! | [`ByteRange`] | Same as using [`std::ops::Range<usize>`] |
+//! | [`ByteStart`] | Start byte index (same as [`.char_indicies()`]) |
+//! | [`ByteEnd`] | End byte index |
+//! | _Tuples are also supported, e.g._ | |
+//! | <code>([Line],)</code> | _Produces the tuple_ |
+//! | <code>([Line], [Col])</code> | _Produces the tuple_ |
+//! | <code>([Line], [Col], [ByteStart], [ByteEnd])</code> | _Produces the tuple_ |
+//! | _etc._ | |
+//!
+//! ## Example - `LineColByteRange`
+//!
+//! ```
+//! use char_positions::{CharPositionsExt, LineColByteRange};
+//!
+//! let text = "Hello 👋\nWorld 🌏\n🦀🦀";
+//!
+//! let mut iter = text
+//!     .char_positions::<LineColByteRange>()
+//!     .map(|(LineColByteRange(line, col, range), c)| (line, col, range, c));
+//!
+//! assert_eq!(iter.next(), Some((1, 1, 0..1, 'H')));
+//! assert_eq!(iter.next(), Some((1, 2, 1..2, 'e')));
+//! assert_eq!(iter.next(), Some((1, 3, 2..3, 'l')));
+//! assert_eq!(iter.next(), Some((1, 4, 3..4, 'l')));
+//! assert_eq!(iter.next(), Some((1, 5, 4..5, 'o')));
+//! assert_eq!(iter.next(), Some((1, 6, 5..6, ' ')));
+//! assert_eq!(iter.next(), Some((1, 7, 6..10, '👋')));
+//! assert_eq!(iter.next(), Some((1, 8, 10..11, '\n')));
+//! assert_eq!(iter.next(), Some((2, 1, 11..12, 'W')));
+//! assert_eq!(iter.next(), Some((2, 2, 12..13, 'o')));
+//! assert_eq!(iter.next(), Some((2, 3, 13..14, 'r')));
+//! assert_eq!(iter.next(), Some((2, 4, 14..15, 'l')));
+//! assert_eq!(iter.next(), Some((2, 5, 15..16, 'd')));
+//! assert_eq!(iter.next(), Some((2, 6, 16..17, ' ')));
+//! assert_eq!(iter.next(), Some((2, 7, 17..21, '🌏')));
+//! assert_eq!(iter.next(), Some((2, 8, 21..22, '\n')));
+//! assert_eq!(iter.next(), Some((3, 1, 22..26, '🦀')));
+//! assert_eq!(iter.next(), Some((3, 2, 26..30, '🦀')));
+//! assert_eq!(iter.next(), None);
+//! ```
+//!
+// Manually linking everything, as `cargo rdme` does not support intralinks
+//!
+//! [`.char_positions()`]: https://docs.rs/char-positions/*/char_positions/trait.CharPositionsExt.html#tymethod.char_positions
+//! [char_positions]: https://docs.rs/char-positions/*/char_positions/trait.CharPositionsExt.html#tymethod.char_positions
+//!
+//! [`LineColByteRange`]: https://docs.rs/char-positions/*/char_positions/struct.LineColByteRange.html
+//! [`LineCol`]: https://docs.rs/char-positions/*/char_positions/struct.LineCol.html
+//! [`Line`]: https://docs.rs/char-positions/*/char_positions/struct.Line.html
+//! [`Col`]: https://docs.rs/char-positions/*/char_positions/struct.Line.html
+//! [`ByteRange`]: https://docs.rs/char-positions/*/char_positions/struct.ByteRange.html
+//! [`ByteStart`]: https://docs.rs/char-positions/*/char_positions/struct.ByteStart.html
+//! [`ByteEnd`]: https://docs.rs/char-positions/*/char_positions/struct.ByteEnd.html
+//!
+//! [LineColByteRange]: https://docs.rs/char-positions/*/char_positions/struct.LineColByteRange.html
+//! [LineCol]: https://docs.rs/char-positions/*/char_positions/struct.LineCol.html
+//! [Line]: https://docs.rs/char-positions/*/char_positions/struct.Line.html
+//! [Col]: https://docs.rs/char-positions/*/char_positions/struct.Col.html
+//! [ByteStart]: https://docs.rs/char-positions/*/char_positions/struct.ByteStart.html
+//! [ByteEnd]: https://docs.rs/char-positions/*/char_positions/struct.ByteEnd.html
+//!
+//! [`.char_indicies()`]: https://doc.rust-lang.org/std/primitive.str.html#method.char_indices
+//!
+//! [`std::ops::Range<usize>`]: https://doc.rust-lang.org/std/ops/struct.Range.html
+
 #![no_std]
 #![forbid(unsafe_code)]
 #![forbid(elided_lifetimes_in_paths)]
@@ -9,6 +129,9 @@ use core::ops::Range;
 use char_ranges::{CharRanges, CharRangesExt};
 
 pub trait CharPositionsExt {
+    /// Returns an iterator over [`char`]s and their positions.
+    ///
+    /// See examples in the [crate root](crate).
     fn char_positions<T>(&self) -> CharPositions<'_, T>
     where
         LineColByteRange: Into<T>;
@@ -24,6 +147,11 @@ impl CharPositionsExt for str {
     }
 }
 
+/// An iterator over [`char`]s and their positions.
+///
+/// Note: Cloning this iterator is essentially a copy.
+///
+/// See examples in the [crate root](crate).
 #[derive(Clone, Debug)]
 pub struct CharPositions<'a, T> {
     iter: CharRanges<'a>,
@@ -74,36 +202,42 @@ where
 
 impl<T> FusedIterator for CharPositions<'_, T> where Self: Iterator {}
 
+/// `Line(line)`
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct Line(
     /// 1-indexed line.
     pub usize,
 );
 
+/// `Col(col)`
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct Col(
     /// 1-indexed column.
     pub usize,
 );
 
+/// `ByteStart(byte_start)`
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct ByteStart(
     /// The start (inclusive) byte positions.
     pub usize,
 );
 
+/// `ByteEnd(byte_end)`
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct ByteEnd(
     /// The end (exclusive) byte position.
     pub usize,
 );
 
+/// `ByteRange(byte_start..byte_end)`
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
 pub struct ByteRange(
     /// The start (inclusive) and end (exclusive) byte positions.
     pub Range<usize>,
 );
 
+/// `LineCol(line, col)`
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct LineCol(
     /// 1-indexed line.
@@ -126,6 +260,7 @@ impl LineCol {
     }
 }
 
+/// `LineColByte(line, col, byte_start)`
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct LineColByte(
     /// 1-indexed line.
@@ -155,6 +290,7 @@ impl LineColByte {
     }
 }
 
+/// `LineColByteRange(line, col, byte_start..byte_end)`
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
 pub struct LineColByteRange(
     /// 1-indexed line.
